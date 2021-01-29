d150d00ee0867ff284e22dd7df3af8aad577f4e8
Viktor <victorosetrov@github.com>
2021-01-29 19:11:02 +0000

    Simple excercices using JS.

---
+++ script.js
@@ -0,0 +1,25 @@
+let markWeight = 78;
+let markHeight = 1.69;
+let johnWeight = 92;
+let johnHeight = 1.95;
+
+markBMI = markWeight / markHeight ** 2;
+johnBMI = johnWeight / johnHeight ** 2;
+console.log(markBMI); 
+console.log(johnBMI);
+
+markHigherBMI = markBMI > johnBMI;
+console.log(markHigherBMI);
+
+markWeight = 95;
+markHeight = 1.88;
+johnWeight = 85;
+johnHeight = 1.76;
+
+markBMI = markWeight / markHeight ** 2;
+johnBMI = johnWeight / johnHeight ** 2;
+console.log(markBMI); 
+console.log(johnBMI);
+
+markHigherBMI = markBMI > johnBMI;
+console.log(markHigherBMI);
