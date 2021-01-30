5330d0fea8d5c78d1c8f32834df958c0b195a75e
Viktor <victorosetrov@github.com>
2021-01-30 19:52:38 +0000

    If/else statements and backticks.

--- script.js
+++ script.js
@@ -11,6 +11,12 @@ console.log(johnBMI);
 markHigherBMI = markBMI > johnBMI;
 console.log(markHigherBMI);
 
+if (markHigherBMI) {
+    console.log(`Mark has higher BMI because he has ${markBMI} in comperison with John because he has ${johnBMI}`)
+} else {
+    console.log(`John has higher BMI because he has ${johnBMI} in comparision with Mark because he has ${markBMI}`)
+};
+
 markWeight = 95;
 markHeight = 1.88;
 johnWeight = 85;
@@ -23,3 +29,36 @@ console.log(johnBMI);
 
 markHigherBMI = markBMI > johnBMI;
 console.log(markHigherBMI);
+
+if (markHigherBMI) {
+    console.log(`Mark has higher BMI because he has ${markBMI} in comperison with John because he has ${johnBMI}`)
+} else {
+    console.log(`John has higher BMI because he has ${johnBMI} in comparision with Mark because he has ${markBMI}`)
+};
+
+const name = 'Victor';
+const dateBirth = 1988;
+const job = 'analyst';
+const currentYear = 2021;
+
+const results = "I'm" + ' ' + name + " " + 'and' + ' ' + "I'm" + " " + (currentYear - dateBirth) + ' ' + 'and' + ' ' + "I'm" + ' ' + 'an' + ' ' + job;
+console.log(results);
+
+const resultsNew = `I'm ${name} and I'm ${currentYear - dateBirth} and I'm an ${job}`;
+console.log(resultsNew);
+
+const age = 17;
+const leftAge = 18 - age
+
+if (age >= 18) {
+    console.log('Sarah is legal')
+} else {
+    console.log(`Sarah is illegal, please wait ${leftAge} years`)
+};
+
+const birthAge = 2005;
+if ((birthAge >= 1901) & (birthAge <= 2000)) {
+    console.log('You are from XX century')
+} else {
+    console.log('You are from XXI century')
+};
