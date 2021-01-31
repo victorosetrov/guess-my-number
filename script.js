c63b523e85384c41b7e1f811c4c0f3699989a0a9
Viktor <victorosetrov@github.com>
2021-01-31 15:32:29 +0000

    Testing conditions, operations, conversion/coercion.

--- script.js
+++ script.js
@@ -68,3 +68,160 @@ if ((birthAge >= 1901) & (birthAge <= 2000)) {
 } else {
     console.log('You are from XXI century')
 };
+
+// Conversion and coercion
+
+// const favorite = prompt('What is your favorite number?')
+// console.log(favorite)
+
+// if (favorite === '23') {
+//     console.log('Your favorite number is 23')
+// } else if (favorite === '7') {
+//     console.log('Your favorite number is 7')
+// } else {
+//     console.log('Your favorite number is different')
+// }
+
+const hasDrivingLicense = true;
+const hasGoodVision = true;
+
+console.log(hasDrivingLicense && hasGoodVision);
+
+let dolphinScore1 = 96;
+let dolphinScore2 = 108;
+let dolphinScore3 = 89;
+
+let koalasScore1 = 88;
+let koalasScore2 = 91;
+let koalasScore3 = 110;
+
+let dolphinAvergae = (dolphinScore1 + dolphinScore2 + dolphinScore3) / 3
+console.log(dolphinAvergae)
+
+let koalasAverage = (koalasScore1 + koalasScore2 + koalasScore3) / 3
+console.log(koalasAverage)
+
+if (dolphinAvergae > koalasAverage) {
+    console.log('Dolphin has higher average in comparison with Koalas')
+} else if (dolphinAvergae = koalasAverage) {
+    console.log('Dolphin has the same average with Koalas')
+} else {
+    console.log('Koalas has higher average in comparison with Dolphin')
+}
+
+if (dolphinScore1 > koalasScore1) {
+    console.log('Dolphin has higher average in comparison with Koalas')
+} else if (dolphinScore1 === koalasScore1) {
+    console.log('Dolphin has the same average with Koalas')
+} else {
+    console.log('Koalas has higher average in comparison with Dolphin')
+}
+
+if (dolphinScore2 > koalasScore2) {
+    console.log('Dolphin has higher average in comparison with Koalas')
+} else if (dolphinScore2 === koalasScore2) {
+    console.log('Dolphin has the same average with Koalas')
+} else {
+    console.log('Koalas has higher average in comparison with Dolphin')
+}
+
+if (dolphinScore3 > koalasScore3) {
+    console.log('Dolphin has higher average in comparison with Koalas')
+} else if (dolphinScore3 === koalasScore3) {
+    console.log('Dolphin has the same average with Koalas')
+} else {
+    console.log('Koalas has higher average in comparison with Dolphin')
+}
+
+dolphinScore1 = 97;
+dolphinScore2 = 112;
+dolphinScore3 = 101;
+
+koalasScore1 = 109;
+koalasScore2 = 95;
+koalasScore3 = 123;
+
+dolphinAvergae = (dolphinScore1 + dolphinScore2 + dolphinScore3) / 3
+console.log(dolphinAvergae)
+
+koalasAverage = (koalasScore1 + koalasScore2 + koalasScore3) / 3
+console.log(koalasAverage)
+
+if (dolphinAvergae > koalasAverage) {
+    console.log('Dolphin has higher average in comparison with Koalas')
+} else if (dolphinAvergae = koalasAverage) {
+    console.log('Dolphin has the same average with Koalas')
+} else {
+    console.log('Koalas has higher average in comparison with Dolphin')
+}
+
+if (dolphinScore1 > koalasScore1) {
+    console.log('Dolphin has higher average in comparison with Koalas')
+} else if (dolphinScore1 === koalasScore1) {
+    console.log('Dolphin has the same average with Koalas')
+} else {
+    console.log('Koalas has higher average in comparison with Dolphin')
+}
+
+if (dolphinScore2 > koalasScore2) {
+    console.log('Dolphin has higher average in comparison with Koalas')
+} else if (dolphinScore2 === koalasScore2) {
+    console.log('Dolphin has the same average with Koalas')
+} else {
+    console.log('Koalas has higher average in comparison with Dolphin')
+}
+
+if (dolphinScore3 > koalasScore3) {
+    console.log('Dolphin has higher average in comparison with Koalas')
+} else if (dolphinScore3 === koalasScore3) {
+    console.log('Dolphin has the same average with Koalas')
+} else {
+    console.log('Koalas has higher average in comparison with Dolphin')
+}
+
+dolphinScore1 = 97;
+dolphinScore2 = 112;
+dolphinScore3 = 101;
+
+koalasScore1 = 109;
+koalasScore2 = 95;
+koalasScore3 = 106;
+
+
+dolphinAvergae = (dolphinScore1 + dolphinScore2 + dolphinScore3) / 3
+console.log(dolphinAvergae)
+
+koalasAverage = (koalasScore1 + koalasScore2 + koalasScore3) / 3
+console.log(koalasAverage)
+
+if (dolphinAvergae > koalasAverage) {
+    console.log('Dolphin has higher average in comparison with Koalas')
+} else if (dolphinAvergae = koalasAverage) {
+    console.log('Dolphin has the same average with Koalas')
+} else {
+    console.log('Koalas has higher average in comparison with Dolphin')
+}
+
+if (dolphinScore1 > koalasScore1) {
+    console.log('Dolphin has higher average in comparison with Koalas')
+} else if (dolphinScore1 === koalasScore1) {
+    console.log('Dolphin has the same average with Koalas')
+} else {
+    console.log('Koalas has higher average in comparison with Dolphin')
+}
+
+if (dolphinScore2 > koalasScore2) {
+    console.log('Dolphin has higher average in comparison with Koalas')
+} else if (dolphinScore2 === koalasScore2) {
+    console.log('Dolphin has the same average with Koalas')
+} else {
+    console.log('Koalas has higher average in comparison with Dolphin')
+}
+
+if (dolphinScore3 > koalasScore3) {
+    console.log('Dolphin has higher average in comparison with Koalas')
+} else if (dolphinScore3 === koalasScore3) {
+    console.log('Dolphin has the same average with Koalas')
+} else {
+    console.log('Koalas has higher average in comparison with Dolphin')
+}
